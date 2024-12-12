d69eb613d3f7cf6d07e84bdc738602174ff69f9a
Uttam <uttam002600@gmail.com>
2024-12-12 12:06:28 +0530

    fix

--- src/assets/components/Dashboard/EmployeeDashboard.jsx
+++ src/assets/components/Dashboard/EmployeeDashboard.jsx
@@ -5,7 +5,7 @@ import TaskList from "../TaskList/TaskList";
 
 const EmployeeDashboard = ({ data, changeUser }) => {
   return (
-    <div className="p-10 bg-[#1C1C1C] h-screen ">
+    <div className="sm:p-10 pt-2 bg-[#1C1C1C] h-screen ">
       <Header changeUser={changeUser} data={data} />
       <TaskListNumbers data={data} />
       <TaskList data={data} />
